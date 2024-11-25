2ca64b3962fce58b5c1d4d02bd1b6bb7fb12b0c1
p@ren <ntimprosper308@gmail.com>
2024-11-25 18:52:09 +0000

    Modify Counter.jsx
    Added useEffect to Counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Counter = () => {
   // The use state
   const [count, setCount] = useState(0);
 
+  // Increases the count
   const increase = () =>{
     setCount(count + 1)
   }
 
+  // Decreases the count
 const decrease = () =>{
     setCount(count - 1)
   }
 
+  useEffect(() => {
+    console.log('The count is:', count);
+  }, [count])
 
   return (
     <div>
       <p>The button has been clicked {count} times</p>
       <button className='bu' onClick={increase}>Increase</button>
       <button className='bu2' onClick={decrease}>Decrease</button>
-
     </div>
   )
 }
