6d242d478357db905099ece4fae81f7a15f305d4
p@ren <ntimprosper308@gmail.com>
2024-11-25 18:02:39 +0000

    Modify Counter.jsx
    Update buttons to increase and decrease count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,16 +4,21 @@ const Counter = () => {
   // The use state
   const [count, setCount] = useState(0);
 
-  /
-  const buttonClicked = () =>{
+  const increase = () =>{
     setCount(count + 1)
+  }
+
+const decrease = () =>{
+    setCount(count - 1)
   }
 
 
   return (
     <div>
       <p>The button has been clicked {count} times</p>
-      <button className='bu' onClick={buttonClicked}>Touch Me</button>
+      <button className='bu' onClick={increase}>Increase</button>
+      <button className='bu2' onClick={decrease}>Decrease</button>
+
     </div>
   )
 }
