365345a8c2da417303322c1496d17ba87adfaa08
p@ren <ntimprosper308@gmail.com>
2024-11-27 17:16:46 +0000

    Add reset button
    Added reset button to reset counter value to 0

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react'
 
 const Counter = () => {
+
+  
   // The use state
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+
+  // Resets the state back to 0
+  const reset = () =>{
+    setCount(0)
+  }
 
   // Increases the count
   const increase = () =>{
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
   // Decreases the count
 const decrease = () =>{
-    setCount(count - 1)
+  setCount(count - 1);
   }
 
   useEffect(() => {
@@ -19,10 +26,11 @@ const decrease = () =>{
   }, [count])
 
   return (
-    <div>
+    <div align>
       <p>The button has been clicked {count} times</p>
       <button className='bu' onClick={increase}>Increase</button>
       <button className='bu2' onClick={decrease}>Decrease</button>
+      <button className='bu3' onClick={reset}>Reset</button>
     </div>
   )
 }
